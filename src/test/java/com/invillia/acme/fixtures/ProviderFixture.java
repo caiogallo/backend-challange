7db31ecd38a582c8d3400103a53ca64eb7310a24
Caio César Gallo <caio.gallo@gmail.com>
2018-11-26 19:04:30 -0200

    fixtures for Provider class

---
+++ src/test/java/com/invillia/acme/fixtures/ProviderFixture.java
@@ -0,0 +1,20 @@
+package com.invillia.acme.fixtures;
+
+import br.com.six2six.fixturefactory.Fixture;
+import br.com.six2six.fixturefactory.Rule;
+import br.com.six2six.fixturefactory.loader.TemplateLoader;
+import com.invillia.acme.address.model.Address;
+import com.invillia.acme.provider.model.Provider;
+
+public class ProviderFixture implements TemplateLoader {
+    public void load(){
+        Fixture.of(Provider.class).addTemplate("valid-without-id", new Rule(){{
+            add("name", random("Provider 1, Provider 2"));
+            add("address", one(Address.class, "valid-without-id"));
+        }});
+
+        Fixture.of(Provider.class).addTemplate("valid").inherits("valid-without-id", new Rule(){{
+            add("id", random(Long.class, range(1L, 10L)));
+        }});
+    }
+}
