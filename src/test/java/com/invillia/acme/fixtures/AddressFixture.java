93cf7d646458ae6305e4c118596fb097451401eb
Caio César Gallo <caio.gallo@gmail.com>
2018-11-26 19:05:03 -0200

    fixtures for Address class

---
+++ src/test/java/com/invillia/acme/fixtures/AddressFixture.java
@@ -0,0 +1,21 @@
+package com.invillia.acme.fixtures;
+
+import br.com.six2six.fixturefactory.Fixture;
+import br.com.six2six.fixturefactory.Rule;
+import br.com.six2six.fixturefactory.loader.TemplateLoader;
+import com.invillia.acme.address.model.Address;
+
+public class AddressFixture implements TemplateLoader {
+    public void load(){
+        Fixture.of(Address.class).addTemplate("valid-without-id", new Rule(){{
+            add("street", random("Rua Pernambuco", "Av Paulista"));
+            add("number", random(100, 200));
+            add("zipCode", random("101000", "120000"));
+            add("complement", random("ao lado do correio", "próximo ao shopping"));
+        }});
+
+        Fixture.of(Address.class).addTemplate("valid").inherits("valid-without-id", new Rule(){{
+            add("id", random(1l, 10l));
+        }});
+    }
+}
