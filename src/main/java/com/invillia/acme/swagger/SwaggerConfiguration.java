df8058e3008cce2572d53ab970aaea5b9008f8e4
Caio César Gallo <caio.gallo@gmail.com>
2018-11-28 11:11:55 -0200

    swagger configuration

---
+++ src/main/java/com/invillia/acme/swagger/SwaggerConfiguration.java
@@ -0,0 +1,58 @@
+package com.invillia.acme.swagger;
+
+import io.swagger.annotations.Api;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.AuthorizationScopeBuilder;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+    @Bean
+    public Docket api() {
+        AuthorizationScope[] authScopes = new AuthorizationScope[1];
+        authScopes[0] = new AuthorizationScopeBuilder()
+                .scope("")
+                .build();
+
+        SecurityReference securityReference = SecurityReference.builder()
+                .reference("basicAuth")
+                .scopes(authScopes)
+                .build();
+
+        ArrayList<SecurityReference> reference = new ArrayList<>(1);
+        reference.add(securityReference);
+
+
+        ArrayList<SecurityContext> securityContexts = new ArrayList<>(1);
+        securityContexts.add(SecurityContext.builder().securityReferences(reference).build());
+
+        ArrayList<SecurityScheme> auth = new ArrayList<>(1);
+        auth.add(new BasicAuth("basicAuth"));
+
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                    .paths(regex("/api*"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Acme API")
+                .version("1.0")
+                .build();
+    }
+
+}
